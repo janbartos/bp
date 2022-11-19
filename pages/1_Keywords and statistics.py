937c1f888e2708f2fd735ed41e5f6a7bf90e1583
janbartos <47862442+janbartos@users.noreply.github.com>
2022-11-19 18:14:10 +0100

    f

--- pages/1_Keywords and statistics.py
+++ pages/1_Keywords and statistics.py
@@ -156,7 +156,7 @@ if len(df_corr[df_corr['fertility'].between(corr[0], corr[1], inclusive="neither
 
 
     ax3.plot(ftr, abline_values, 'b')
-    ax3.plot(ftr, key_data, 'o', label = 'original data')
+    ax3.plot(ftr, key_data, 'ro', label = 'original data')
     #ax3.plot(ftr, intercept + (slope * ftr), "r", label = "fitted line")
 
     st.pyplot(fig2)
