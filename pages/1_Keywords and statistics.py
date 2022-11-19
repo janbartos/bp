2d8df4efe1e8482bc8054ebd82d0c71a994f911d
janbartos <47862442+janbartos@users.noreply.github.com>
2022-11-19 17:53:17 +0100

    Update 1_Keywords and statistics.py

--- pages/1_Keywords and statistics.py
+++ pages/1_Keywords and statistics.py
@@ -149,6 +149,6 @@ if len(df_corr[df_corr['fertility'].between(corr[0], corr[1], inclusive="neither
 
     fig2, ax3 = plt.subplots()
     ax3.plot(ftr, key_data, 'o', label = 'original data')
-    ax3.plot(ftr, intercept, slope * ftr, "r", label = "fitted line")
+    ax3.plot(ftr, intercept + slope * ftr, "r", label = "fitted line")
 
     st.pyplot(fig2)
