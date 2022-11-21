79ffccb5913bc11e1762ac15c7f326cb22fef25a
janbartos <47862442+janbartos@users.noreply.github.com>
2022-11-21 19:38:38 +0100

    Interesting outliers headers

--- pages/3_Interesting outliers.py
+++ pages/3_Interesting outliers.py
@@ -112,7 +112,7 @@ chart_data = pd.DataFrame({
 })
 
 bar_chart = alt.Chart(chart_data).mark_bar().encode(
-        x="Year:O",
+        x="Year:T",
         y="Results:Q",
         color="Party:N"
     )
