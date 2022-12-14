6e428b0478c731d168918115614501a1ec66fe9c
janbartos <47862442+janbartos@users.noreply.github.com>
2022-12-14 15:54:43 +0100

    repair regression first page

--- pages/1_Keywords and statistics.py
+++ pages/1_Keywords and statistics.py
@@ -159,6 +159,7 @@ if len(df_corr[df_corr['fertility'].between(corr[0], corr[1], inclusive="neither
     ax3.plot(key_data, ftr, 'ro', label = 'original data')
     ax3.plot(x, yfit1, label="y=%5.f*x^2+%5.f*x+%5.3f" % tuple(params))
     ax3.legend(["FTR in " + country, str(keyword)])
+    ax3.legend(loc='best', fancybox=True, shadow=True)
     ax3.grid()
     ax3.set_xlabel(r"Searched")
     ax3.set_ylabel(r"Fertility rate")
