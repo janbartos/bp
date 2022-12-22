142cce070063bc53f6ea7dee78b977438fa6ed9b
janbartos <47862442+janbartos@users.noreply.github.com>
2022-12-22 19:46:33 +0100

    update files

--- pages/2_Categories and statistics.py
+++ pages/2_Categories and statistics.py
@@ -163,7 +163,9 @@ ax3.plot(key_data, ftr, 'ro', label='original data')
 #ax3.plot(x, yfit1, label="y=%5.f*x^2+%5.f*x+%5.3f" % tuple(params))
 #ax3.plot(polyline, yfit1, label="y=%f*x^2+%f*x+%f" % tuple(params))
 #ax3.plot(polyline, model(polyline), label="y=%f*x^2+%f*x+%f" % tuple(params))
-ax3.plot(polyline, model(polyline), label="y=%f*x^2+%f*x+%f" % (model[2], model[1], model[0]))
+
+
+ax3.plot(polyline, model(polyline), label="y=%7.f*x^2+%7.f*x+%7.3f" % (model[2], model[1], model[0]))
 #ax3.legend(["Original data", "Regressive line"])
 ax3.legend(loc='best', fancybox=True, shadow=True)
 ax3.grid()
@@ -173,14 +175,6 @@ ax3.set_ylabel(r"Fertility rate")
 st.pyplot(fig2)
 
 
-st.subheader(model[0])
-st.subheader(model[1])
-st.subheader(model[2])
-st.subheader(model[3])
-
-st.subheader(a)
-st.subheader(b)
-st.subheader(c)
 
 
 
